0385dfd95f4468e2392a7c0daa947db16ee5d21f
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-08-15 11:14:17 +0200

    1->I

--- src/roman_numerals/core.clj
+++ src/roman_numerals/core.clj
@@ -1,1 +1,4 @@
 (ns roman-numerals.core)
+
+(defn arabic->roman [arabic]
+  "I")
--- test/roman_numerals/core_test.clj
+++ test/roman_numerals/core_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [roman-numerals.core :refer :all]))
 
-(deftest a-test1
-  (testing "Basic tests"
-    ))
+(deftest test-arabic-to-roman-numeral-conversion
+  (testing "arabic 1 is roman I"
+    (is (= "I" (arabic->roman 1)))))
