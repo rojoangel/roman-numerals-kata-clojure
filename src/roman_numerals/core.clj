4af4c5a91827a903140d8a90a64e8b74d637c01d
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-08-15 11:47:15 +0200

    refactor: introduce 'roman-for' map

--- src/roman_numerals/core.clj
+++ src/roman_numerals/core.clj
@@ -1,12 +1,17 @@
 (ns roman-numerals.core)
 
+(def roman-for
+  {5 "V"
+   4 "IV"
+   1 "I"})
+
 (defn arabic->roman [arabic]
   (loop [remaining arabic
          roman ""]
     (if (= remaining 5)
-      (recur (- remaining 5) (str roman "V"))
+      (recur (- remaining 5) (str roman (get roman-for 5)))
       (if (= remaining 4)
-        (recur (- remaining 4) (str roman "IV"))
+        (recur (- remaining 4) (str roman (get roman-for 4)))
         (if (= remaining 0)
           roman
-          (recur (- remaining 1) (str roman "I")))))))
+          (recur (- remaining 1) (str roman (get roman-for 1))))))))
