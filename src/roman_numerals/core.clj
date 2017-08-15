b278ae033497adcd32174ead69fb23012c93a4ed
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-08-15 18:27:01 +0200

    refactor: introduce multi-arity to reduce duplication

--- src/roman_numerals/core.clj
+++ src/roman_numerals/core.clj
@@ -5,14 +5,13 @@
    4 "IV"
    1 "I"})
 
-(defn arabic->roman [arabic]
-  (let [roman ""]
-    (loop [remaining arabic
-           roman roman]
-      (if (>= remaining 5)
-        (recur (- remaining 5) (str roman (get arabic-to-roman-digits 5)))
-        (if (>= remaining 4)
-          (recur (- remaining 4) (str roman (get arabic-to-roman-digits 4)))
-          (if (>= remaining 1)
-            (recur (- remaining 1) (str roman (get arabic-to-roman-digits 1)))
-            roman))))))
+(defn arabic->roman
+  ([arabic]
+   (arabic->roman arabic "" arabic-to-roman-digits))
+  ([arabic roman digits-to-process]
+   (let [[arabic-digit roman-digit] (first digits-to-process)]
+     (if (>= arabic arabic-digit)
+       (recur (- arabic arabic-digit) (str roman roman-digit) digits-to-process)
+       (if (> arabic 0)
+         (recur arabic roman (rest digits-to-process))
+         roman)))))
