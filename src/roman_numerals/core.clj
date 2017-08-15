6d79f4d1a74f1f11c2ee7cd3a11c7eb06c294d59
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-08-15 11:17:25 +0200

    3->III

--- src/roman_numerals/core.clj
+++ src/roman_numerals/core.clj
@@ -1,6 +1,8 @@
 (ns roman-numerals.core)
 
 (defn arabic->roman [arabic]
-  (if (= arabic 2)
-    "II"
-    "I"))
+  (if (= arabic 3)
+    "III"
+    (if (= arabic 2)
+      "II"
+      "I")))
--- test/roman_numerals/core_test.clj
+++ test/roman_numerals/core_test.clj
@@ -6,4 +6,6 @@
   (testing "arabic 1 is roman I"
     (is (= "I" (arabic->roman 1))))
   (testing "arabic 2 is roman II"
-    (is (= "II" (arabic->roman 2)))))
+    (is (= "II" (arabic->roman 2))))
+  (testing "arabic 3 is roman III"
+    (is (= "III" (arabic->roman 3)))))
