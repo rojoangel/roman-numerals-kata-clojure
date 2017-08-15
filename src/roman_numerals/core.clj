49ff7bd48a7247701093f9918bf7d126140699b3
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-08-15 17:43:51 +0200

    refactor: extract roman initialization

--- src/roman_numerals/core.clj
+++ src/roman_numerals/core.clj
@@ -6,12 +6,13 @@
    1 "I"})
 
 (defn arabic->roman [arabic]
-  (loop [remaining arabic
-         roman ""]
-    (if (>= remaining 5)
-      (recur (- remaining 5) (str roman (get arabic-to-roman-digits 5)))
-      (if (>= remaining 4)
-        (recur (- remaining 4) (str roman (get arabic-to-roman-digits 4)))
-        (if (>= remaining 1)
-          (recur (- remaining 1) (str roman (get arabic-to-roman-digits 1)))
-          roman)))))
+  (let [roman ""]
+    (loop [remaining arabic
+           roman roman]
+      (if (>= remaining 5)
+        (recur (- remaining 5) (str roman (get arabic-to-roman-digits 5)))
+        (if (>= remaining 4)
+          (recur (- remaining 4) (str roman (get arabic-to-roman-digits 4)))
+          (if (>= remaining 1)
+            (recur (- remaining 1) (str roman (get arabic-to-roman-digits 1)))
+            roman))))))
